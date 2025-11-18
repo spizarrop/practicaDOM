409780dd72b875305dc56afc5c428d52dfbd4fc4
Santiago Pizarro <santiagopizarropizarro.guadalupe@alumnado.fundacionloyola.net>
2025-11-18 10:06:13 +0100

    v1

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 // Ejercicio 1
+const boton = document.querySelector("#cambiar");
+
+if(boton)
+{
+    boton.addEventListener("click", cambiarTexto);
+}
+
+function cambiarTexto(){
+    document.querySelector("#titulo").innerHTML
+    const titulo = document.querySelector("#titulo");
+    titulo.innerHTML = "DOM modificado"
+}
 
 // Ejercicio 2
 // Ejercicio 3
